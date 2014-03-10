dd23edb067c82bde362d11b60478e1cea409bccb
Matthew Tole <matthewtole@gmail.com>
2014-03-10 16:41:35 +0000

    Update sample with comments and to take advantage of the new 0.3.6 features.

--- catapult-sample/src/main/java/com/matthewtole/android/catapult_sample/MainActivity.java
+++ catapult-sample/src/main/java/com/matthewtole/android/catapult_sample/MainActivity.java
@@ -40,8 +40,14 @@ public class MainActivity extends Activity {
         mBtnPick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Show the Catapult app picker and get the details for the app.
+
+                // Create a new intent with the Tasker Edit action.
                 Intent intent = new Intent(TASKER_EDIT);
+                // Set the component to be the Catapult Edit Activity
                 intent.setComponent(new ComponentName(CATAPULT_PACKAGE, CATAPULT_EDIT));
+                // Create a new bundle and set the type of the Catapult action to be "pebble-app"
+                // This will skip the Catapult menu that includes Notifications.
                 Bundle bundle = new Bundle();
                 bundle.putString("type", CATAPULT_TYPE_APP);
                 intent.putExtra(TASKER_BUNDLE, bundle);
@@ -53,9 +59,15 @@ public class MainActivity extends Activity {
         mBtnLaunch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Send the bundle we got back from Catapult at Catapult.
+
+                // Create a new intent with the Tasker Edit action.
                 Intent intent = new Intent(TASKER_FIRE);
+                // Set the component to be the Catapult Fire Receiver
                 intent.setComponent(new ComponentName(CATAPULT_PACKAGE, CATAPULT_FIRE));
+                // Add the bundle we got from the Edit Activity.
                 intent.putExtra(TASKER_BUNDLE, mBundle);
+                // Send the broadcast!
                 sendBroadcast(intent);
             }
         });
@@ -65,9 +77,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_CATAPULT) {
-            String blurb = data.getStringExtra(TASKER_BLURB);
+            // Grab the bundle of Catapult data from the result.
             mBundle = data.getBundleExtra(TASKER_BUNDLE);
-            mBtnLaunch.setText(blurb);
+            // The bundle contains the name of the app, and its UUID.
+            mBtnLaunch.setText("Launch '" + mBundle.getString("name") + "'!");
             mBtnLaunch.setEnabled(true);
         }
     }
